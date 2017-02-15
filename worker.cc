7aca21ac8610bbe480d91e522a7c3a13843f9ddd
Tekin Bicer <bicer@anl.gov>
2017-02-15 15:46:58 +0000

    worker is configured for c++11 spec

--- worker.cc
+++ worker.cc
@@ -6,15 +6,17 @@
 
 
 #include "zhelpers.hpp"
+#include "mpi.h"
+#include <string>
 
-#define REP_ADDR "tcp://164.54.143.3"
+#define REP_ADDR "tcp://164.54.143.3:"
 #define S_PORT 5560
 
 int main (int argc, char *argv[])
 {
   // Setup MPI
   int my_rank, world_size;
-  MPI_Init(argc, argv);
+  MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
@@ -23,25 +25,23 @@ int main (int argc, char *argv[])
   std::stringstream addr_stream;
   std::string rep_addr(REP_ADDR);
   int starting_port = S_PORT;
-  addr_str_stream << rep_addr << (starting_port+my_rank);
+  addr_stream << rep_addr << (starting_port+my_rank);
   zmq::socket_t socket_rep(context, ZMQ_REP);
-  socket_rep.connect(addr_str_stream);
-
+  socket_rep.connect(addr_stream.str());
 
   // Wait for the initial greeting
-  std::string msg_rep = s_recv (responder);
+  std::string msg_rep = s_recv(socket_rep);
   std::cout << "Client says: " << msg_rep << std::endl;
 
   // Introduce yourself
-  std::string msg = std::to_string(my_rank);
-  s_send(socket_rep, msg);
+  s_send(socket_rep, std::to_string((long long)my_rank));
 
   // Check if server has any projection
   msg_rep = s_recv(socket_rep);
   std::cout << "Rep socket msg=" << msg_rep << std::endl;
 
   // Say you received it
-  msg = std::string(std::to_string(my_rank) + "I received it");
+  std::string msg = std::string(std::to_string((long long)my_rank) + ": I received it");
   s_send(socket_rep, msg);
   
   MPI_Finalize();
