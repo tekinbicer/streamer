1c8319255a370f37187fa6e632a6c6dedf2c62a3
Tekin Bicer <bicer@anl.gov>
2017-02-17 17:19:56 -0600

    zmq messages were created and distributed to ranks. finish signal to the workers need to be sent.

--- server.c
+++ server.c
@@ -1,7 +1,55 @@
 //   Reply server in C
-
-
 #include "zhelpers.h"
+
+#define N 1024
+#define P 180
+
+typedef struct {
+  float theta; // theta value of this projection
+  int beg_sinogram; // beginning sinogram id
+  int n_sinogram; // # Sinograms
+  // int nprojs=1; // Always projection by projection
+  int n_rays_per_proj_col; // # rays per projection column
+  int16_t *data;  // real projection data
+                  // number of rays in data=n_sinogram*n_rays_per_proj_col
+} tomo_msg_t;
+
+/*
+ * data: pointer to generated data [projection][column][column]
+ * dims: dimensions of the data, consists of 3 int values
+ * theta: pointer to theta values of projections
+ * n_procs: number of processes
+ *
+ * returns msgs to be sent to the processes. tomo_msg_t[P][ranks_msg]
+ * */
+tomo_msg_t** generate_msgs(int16_t *data, int *dims, float *theta, int n_ranks){
+  int nsin = dims[1]/n_ranks;
+  int c_remaining = dims[1]%n_ranks;
+
+  tomo_msg_t **msgs = malloc(P*sizeof(*msgs));
+  for(int i=0; i<P; ++i) msgs[i] = malloc(nranks*sizeof(*msgs));
+
+  curr_sinogram_id = 0;
+  for(int i=0; i<P; ++i){
+    int remaining = c_remaining;
+    curr_sinogram_id = 0;
+
+    for(int j=0; j<nranks; ++j){
+      msgs[i][j].theta = theta[i];
+      msgs[i][j].beg_sinogram = curr_sinogram_id;
+      int r = ((remaining--) > 0) ? 1 : 0;
+      msgs[i][j].n_sinogram = nsin+r;
+      msgs[i][j].n_rays_per_proj_col = dims[2];
+      size_t msg_size = sizeof(*data)*(nsin+r)*dims[2];
+      msgs[i][j].data = malloc(msg_size);
+      memcpy(msgs[i][j].data, (data+i*dims[2]*dims[2]) + // Current projection
+                                     curr_sinogram_id*dims[2], 
+                                     msg_size;  // Current sinogram
+      curr_sinogram_id += nsin+r;
+    }
+  }
+  return msgs;
+}
 
 int main (int argc, char *argv[])
 {
@@ -15,6 +63,20 @@ int main (int argc, char *argv[])
 
   void *context = zmq_ctx_new();
   void **workers = malloc(n_workers*sizeof(workers));
+
+  int dims[3]= {P, N, N};
+  int16_t *data=malloc(sizeof(*data)*P*N*N);
+  float theta[P];
+  for(int i=0; i<P; ++i)
+    for(int j=0; j<N; ++j)
+      for(int k=0; k<N; ++k) data[i*N*N+j*N+k]=k;
+
+  for(int i=0; i<P; ++i) theta[i]=(360./P)*i;
+  
+  // Prepare messages
+  tomo_msg_t **msgs = generate_msgs(data, dims, theta, n_workers);
+  free(data);
+  free(theta);
 
   // Check workers
   for(int i=0; i<n_workers; ++i){
@@ -33,20 +95,36 @@ int main (int argc, char *argv[])
    free(str);
   }
 
+  // TODO: Below two loop is not implemented at client side yet!!
   // At this point I know every one is alive
-  // Send msg/projections
-  for(int i=0; i<n_workers; ++i){
-    char msg[64];
-    snprintf(msg, 64, "socket=%d worker_rank=%d; some projections!!", i, worker_ids[i]);
-    s_send(workers[i], msg);
-  }
+  // Receive/Send msg/projections
+  for(int i=0; i<dims[0]; ++i) {  // For each incoming projection
+    for(int j=0; j<n_workers; ++j){ // For each partitioned projection sinogram
+      // Assume data is ready
+      // Prepare zmq message
+      zmq_msg_t msg;
+      size_t msg_size = sizeof(msgs[i][j]) +            // struct size
+                        msgs[i][j].n_sinogram*          // remaining data size
+                        msgs[i][j].n_rays_per_proj_col*
+                        sizeof(*(msgs[i][j].data));
+      zmq_msg_init_size(&msg, msg_size); 
+      memcpy(zmq_msg_data(&msg), msgs[i][j], msg_size);
+      zmq_send(workers[i], msg, 0);
+      // char msg[64];
+      // snprintf(msg, 64, "socket=%d worker_rank=%d; some projections!!", i, worker_ids[i]);
+      // s_send(workers[i], msg);
+    }
 
-  // Check if they received it
-  for(int i=0; i<n_workers; ++i){
-   char *str = s_recv(workers[i]);
-   printf("worker rank=%d on socket=%d replied: %s\n", worker_ids[i], i, str);
-   free(str);
+    // Check if workers received their corresponding projection rows
+    for(int i=0; i<n_workers; ++i){
+     char *str = s_recv(workers[i]);
+     printf("worker rank=%d on socket=%d replied: %s\n", worker_ids[i], i, str);
+     free(str);
+    }
+    // Move on to next projection
   }
+  // All the projections are finished
+  // TODO: Send a finished signal to the workers before closing the sockets
 
   for(int i=0; i<n_workers; ++i){
     zmq_close (workers[i]);
